52ebac29f64da0c780e4d5810851520ea87d195b
= <kamaljohnson12345@gmail.com>
2018-08-08 02:14:49 +0530

    Initiating the levelCard via script

--- cubix_Packman/Assets/Scripts/LevelMap/LevelMapUI.cs
+++ cubix_Packman/Assets/Scripts/LevelMap/LevelMapUI.cs
@@ -1,12 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class LevelMapUI : MonoBehaviour {
-
-    //TODO call this script when the player selects a level 
-    void selectCard()
+public class LevelMapUI : MonoBehaviour
+{
+    public GameObject LevelCardPrefab;
+    public Transform LevelCardParentTrasform;
+    public List<GameObject> AllLevelCards = new List<GameObject>();
+    private void Start()
     {
-        
+        LoadMap();        
+    }
+
+    public void LoadMap()
+    {
+        Debug.Log("called the funtion");
+        int i = 0;
+        string levelName = "level_";
+        string Directory;
+        //BinaryFormatter BF = new BinaryFormatter();
+        string jsonString;
+        while (true)
+        {
+            levelName += i.ToString();
+            Debug.Log("level : " + levelName);
+            Directory = Application.streamingAssetsPath + "/Levels/" + levelName;
+            i++;
+            if (System.IO.File.Exists(Directory))
+            {
+                InitiateLevelCard(levelName);
+            }
+            else
+            {
+                return;
+            }
+            levelName = levelName.Split('_')[0] + "_";
+        }
+    }
+
+    public void InitiateLevelCard(string cardName)
+    {
+        Debug.Log("instantiated the card");
+        AllLevelCards.Add(Instantiate(LevelCardPrefab, LevelCardParentTrasform));
+    }
+
+    public void DeleteAllCards()
+    {
+        for (int i = 0; i < AllLevelCards.Count; i++)
+        {
+            Destroy(AllLevelCards[i]);
+        }
+        AllLevelCards = new List<GameObject>();
     }
 }
