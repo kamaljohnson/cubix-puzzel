977e78540b86431403523aeac4ce06ec8d29092e
= <kamaljohnson12345@gmail.com>
2018-08-11 12:17:46 +0530

    added prefab type addons to the game

---
+++ cubix_Packman/Assets/Scripts/GameAddOns/AddOnManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddOnManager : MonoBehaviour
+{
+
+    public static int NoOfPoints = 0;
+
+    public static void addPoints()
+    {
+        NoOfPoints++;
+    }
+    
+}
