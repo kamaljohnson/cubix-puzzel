77f9e257348715bbf32d46b9ab3cd61ef66a6a17
= <kamaljohnson12345@gmail.com>
2018-08-23 22:38:30 +0530

    basic season UI and level UI

---
+++ cubix_Packman/Assets/Scripts/Season/SeasonCardButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonCardButton : MonoBehaviour {
+
+	public int SeasonIndex;
+
+	public void SeasonButtonOnClick()
+	{
+		SeasonManager.CurrentSeason = SeasonIndex;
+		SeasonManager.SelectSeason();
+	}	// Use this for initialization
+}
--- cubix_Packman/Assets/Scripts/Season/SeasonManager.cs
+++ cubix_Packman/Assets/Scripts/Season/SeasonManager.cs
@@ -1,33 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 using UnityEngine.SceneManagement;
 
 public class SeasonManager : MonoBehaviour
 {
     private static bool _created = false;
 
+    public int NumberOfSeasons = 5;
     public static int CurrentSeason;
     public static int NumberOfLevelsInSeason;
 
+    public GameObject SeasonCardPrefab;
+    public Transform SeasonCardParentTrasform;
+    List<GameObject> AllSeasonCards = new List<GameObject>();
+
     void Awake()
     {
-        CurrentSeason = 1;
         if (!_created)
         {
             DontDestroyOnLoad(this.gameObject);
             _created = true;
         }
-        SelectSeason();
+        for(var season = 1; season <= NumberOfSeasons; season++)
+            InitiateSeasonCard(season);
+
     }
 
-    public void SelectSeason()
+    public static void SelectSeason()
     {
-     
+
         var i = 1;
         var levelName = string.Format("level_{0}_", CurrentSeason.ToString());
         NumberOfLevelsInSeason = 0;
-        
+
         while (true)
         {
             levelName += i.ToString();
@@ -42,10 +49,18 @@ public class SeasonManager : MonoBehaviour
             {
                 break;
             }
+
             levelName = string.Format("level_{0}_", CurrentSeason.ToString());
         }
-        
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    
+        
+    public void InitiateSeasonCard(int cardIndex)
+    {
+        GameObject tempObj = Instantiate(SeasonCardPrefab, SeasonCardParentTrasform);
+        tempObj.GetComponent<SeasonCardButton>().SeasonIndex = cardIndex;
+        AllSeasonCards.Add(tempObj);
+        
+    }
 }
